2cec7b6a0696e154acbb6d3fc1e0ed088943af7e
Luis <luispacu10@hotmail.com>
2025-06-17 18:12:00 +0200

    fix: authMiddleware sin cookies

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-
 function authMiddleware(req, res, next) {
-  const {token} = req.cookies;
-  if (!token) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Token no proporcionado' });
   }
+
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
